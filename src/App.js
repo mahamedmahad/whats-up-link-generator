191afc6ff43ae1a93b04c0cf06eee70c3213430c
Mahamed Mahad <mahameddeeq26@hotmail.com>
2022-03-19 04:39:52 +0100

    styled the link form!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+//icons
+import {AiFillPhone} from "react-icons/ai";
+import {BiCopy} from "react-icons/bi";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (<div className="App">
+            <div className={"wrapper"}>
+                <div className="iconContainer">
+                    <h2>Whats App Link Generator</h2>
+                    <AiFillPhone
+                        size={40}
+                        color={"#38b000"}
+                    />
+                </div>
+                <form action="" className={"form "}>
+                    <label>Enter Your Phone</label>
+                    <input
+                        type="text"
+                        placeholder="Enter your Phone"
+                        name={"phone"}
+                        className={"input"}
+                    />
+                    <label>Enter Your Message</label>
+                    <textarea
+                        placeholder={"Enter Your Message"}
+                        cols={50}
+                        rows={2}
+                        className={"textarea "}
+                        name={"message"}
+
+                    />
+                    {/***Button-******/}
+                    <button className={"onSubmitBtn"}>Generate a Link</button>
+
+                    <div className={"urlContainer"}>
+                        <input
+                            readOnly={true}
+                            type="text"
+                            name={"link-url"}
+                        />
+                        <span className={"copy"}>
+                            Copy
+                            <BiCopy/>
+                        </span>
+
+                    </div>
+                </form>
+            </div>
+
+        </div>);
 }
 
 export default App;
